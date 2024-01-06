5d19bc6416990c7a0289877784a94eecb5f96333
定世荷 <>
2024-01-06 18:40:26 +0800

    可用username password 登入，role還沒加上去

--- MVC/controller.php
+++ MVC/controller.php
@@ -2,31 +2,38 @@
 require 'model.php';
 
 $action = $_POST['action'];
+$username = $_POST['username'];
+$password = $_POST['password']; // 使用 "password" 作為表單欄位的名稱
 
 if ($action === 'login') {
-     $username = $_POST['username'];
-     $pwd = $_POST['pwd'];
+      $username = $_POST['username'];
+      $password = $_POST['password']; // 使用 "password" 作為表單欄位的名稱
     
-     $model = new UserModel();
-     $user = $model->getUserByUsername($username);
+      $model = new UserModel();
+      $user = $model->getUserByUsername($username);
     
-     if ($user && pwd_verify($pwd, $user['pwd'])) {
-         // 登入成功，跳到購物頁面
-         echo '登入成功！ ';
-     } else {
-         echo '登入失敗，請檢查使用者名稱和密碼。 ';
+      if ($user && password_verify($password, $user['password'])) {
+          // 登入成功，跳到購物頁面
+          $url = "http://localhost/MVC/MVC/clientView.html";
+          echo "<script type='text/javascript'>";
+          echo "window.location.href='$url'";
+          echo "</script>";
+                   }
+     else{
+          echo '登錄失敗，請檢查使用者名稱和密碼。 ';
      }
 } elseif ($action === 'register') {
-     $username = $_POST['username'];
-     $pwd = pwd_hash($_POST['pwd'], pwd_DEFAULT);
+      $username = $_POST['username'];
+      $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // 使用 "password" 作為表單欄位的名稱
     
-     $model = new UserModel();
+      $model = new UserModel();
     
-     if ($model->getUserByUsername($username)) {
-         echo '使用者名稱已存在，請選擇其他使用者名稱。 ';
-     } else {
-         $model->insertUser($username, $pwd);
-         echo '註冊成功！ ';
-     }
+      if ($model->getUserByUsername($username)) {
+          echo '使用者名稱已存在，請選擇其他使用者名稱。 ';
+      } else {
+          $model->insertUser($username, $password);
+          header("Location: http://localhost/MVC/MVC/login.html?message=登入成功！");
+          exit();
+                }
 }
 ?>
--- MVC/model.php
+++ MVC/model.php
@@ -2,10 +2,12 @@
 class UserModel {
     private $conn;
 
+    
     public function __construct() {
         // 建立一個新的PDO（PHP資料物件）實例來建立資料庫連接
         // PDO是PHP用於存取資料庫的標準接口
         // 參數1: 資料庫的主機名稱與資料庫名，格式為 "mysql:host=主機名稱;dbname=資料庫名稱"
+        
         $this->conn = new PDO("mysql:host=localhost;dbname=test", "root", "root");
    
         // 設定PDO的錯誤處理模式為拋出例外（Exception）
@@ -19,9 +21,9 @@ class UserModel {
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function insertUser($username, $pwd) {
-        $stmt = $this->conn->prepare("INSERT INTO user (username, pwd) VALUES (?, ?)");
-        $stmt->execute([$username, $pwd]);
+    public function insertUser($username, $password) {
+        $stmt = $this->conn->prepare("INSERT INTO user (username, password) VALUES (?, ?)");
+        $stmt->execute([$username, $password]);
     }
 }
 ?>
