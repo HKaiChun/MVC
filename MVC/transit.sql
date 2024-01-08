cc806c03e369ff92047cd61c68ea033979e05bdf
LAPTOP-244SK7RO\user <cbdptspt@gmail.com>
2024-01-08 10:00:31 +0800

    新增運送中訂單資料庫

---
+++ MVC/transit.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2024 年 01 月 08 日 02:58
+-- 伺服器版本： 10.4.28-MariaDB
+-- PHP 版本： 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `transit`
+--
+
+CREATE TABLE `transit` (
+  `id` int(10) NOT NULL,
+  `pName` varchar(30) NOT NULL,
+  `price` int(11) NOT NULL,
+  `num` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `merchant` varchar(30) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `username` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `transit`
+--
+ALTER TABLE `transit`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `transit`
+--
+ALTER TABLE `transit`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
