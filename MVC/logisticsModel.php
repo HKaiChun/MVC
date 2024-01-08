34cea0e64f213accf97c188b33f025d070fa0c7d
定世荷 <>
2024-01-08 13:11:05 +0800

    no message

--- MVC/logisticsModel.php
+++ MVC/logisticsModel.php
@@ -4,7 +4,7 @@ require('dbconfig.php');
 function getOrderList() //列出客戶訂單
 {
 	global $db;
-	$sql = "SELECT * FROM `order` WHERE status='已送達' OR status='寄送中' OR status='已寄送'";
+	$sql = "SELECT * FROM `transit` WHERE status='已送達' OR status='寄送中' OR status='已寄送'";
 	$stmt = mysqli_prepare($db, $sql ); //precompile sql指令，建立statement 物件，以便執行SQL
 	mysqli_stmt_execute($stmt); //執行SQL
 	$result = mysqli_stmt_get_result($stmt); //取得查詢結果
@@ -21,7 +21,7 @@ function markOrderDelivered($id) //已送達
 {
     global $db;
     $status = '已送達';
-    $sql = "UPDATE `order` SET status=? WHERE id=?";
+    $sql = "UPDATE `transit` SET status=? WHERE id=?";
     $stmt = mysqli_prepare($db, $sql);
     mysqli_stmt_bind_param($stmt, "si", $status, $id);
     mysqli_stmt_execute($stmt);
@@ -40,7 +40,7 @@ function markOrderShipped($id) //已寄送
     
     if ($currentStatus !== '已送達') {
         $status = '已寄送';
-        $sql = "UPDATE `order` SET status=? WHERE id=?";
+        $sql = "UPDATE `transit` SET status=? WHERE id=?";
         $stmt = mysqli_prepare($db, $sql);
         mysqli_stmt_bind_param($stmt, "si", $status, $id);
         mysqli_stmt_execute($stmt);
@@ -52,7 +52,7 @@ function markOrderShipped($id) //已寄送
 
 function getCurrentStatus($id) {
     global $db;
-    $sql = "SELECT status FROM `order` WHERE id = ?";
+    $sql = "SELECT status FROM `transit` WHERE id = ?";
     $stmt = mysqli_prepare($db, $sql);
 
     if (!$stmt) {
